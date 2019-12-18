9070ee84090b8635d502a2fef0467bd26a286adf
cfn1234 <435450916@qq.com>
2019-12-18 15:28:07 +0800

    Alibaba服务注册   --cfn

---
+++ alibaba-nacos/src/test/java/com/example/alibabanacos/AlibabaNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alibabanacos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
