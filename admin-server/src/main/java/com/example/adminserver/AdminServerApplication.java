94560ab4aa9d5f807ad155e451e6b5f50add34b1
caofengnian <435450916@qq.com>
2020-04-01 10:10:51 +0800

    测试 -cfn

--- admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
@@ -1,6 +1,7 @@
 package com.example.adminserver;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,16 +9,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @author caofengnian
  */
+@Slf4j
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
+		log.info("");
 		SpringApplication.run(AdminServerApplication.class, args);
-		if(false){
-		
-		}
 	}
 	
 }
